7c5456f2dad2fe120efdebf2eb1bc8256133be7c
Jake Gadaleta <jake.gads@gmail.com>
2019-11-21 20:43:54 -0500

    added all of the regression functions

--- Practice/mobile.py
+++ Practice/mobile.py
@@ -22,62 +22,89 @@ warnings.simplefilter(action='ignore', category=FutureWarning)
 
 
 class Accuracy:
-    def __init__(self, X, y, k, accuracy_score):
+    def __init__(self, X, y, degree, accuracy_score):
         self.X = X
         self.y = y
-        self.k = k
+        self.degree = degree
         self.accuracy_score = accuracy_score
 
-    def remake(self, X, y, k, accuracy_score):
+    def remake(self, X, y, degree, accuracy_score):
         self.X = X
         self.y = y
-        self.k = k
+        self.degree = degree
         self.accuracy_score = accuracy_score
 
     def printable(self):
-        return f"X:{self.X}, y:{self.y}, k:{self.k}, score: {self.accuracy_score}"
+        return f"X:{self.X}, y:{self.y}, degree:{self.degree}, score: {self.accuracy_score}"
 
 
-def convert_index(data, y):
-    if not isinstance(y, int):
-        for i in range(len(data)):
-            if data[i] == y:
-                y = i
-                return y
-        if not isinstance(y, int):
-            print("Failed to generate a index for y")
-            exit()
-
-    return y
-
-def poly_regression(X_loc, y_loc, file, max_degree=10, supress_text=False):
-
-    # prepares the file
+def clean_data(file):
     dataset = pd.read_csv(file)
     dataset = dataset.dropna()
     dataset = dataset.reset_index(drop=True)
 
-    # sets my indicies
-    if isinstance(X_loc, list):
-        for i in range(len(X_loc)):
-            X_loc[i] = convert_index(dataset.colmns, X_loc[i])
-    else:
-        X_loc = convert_index(dataset.columns, X_loc)
-    y_loc = convert_index(dataset.columns, y_loc)
+    # no encodeing needed
 
-    # generate the data
+    return dataset
+
+
+def polynomial(file, X_loc, y_loc, max_degree=20):
+    dataset = clean_data(file)
+    accuracy = Accuracy(0, 0, 0, 0)
+
     if not isinstance(X_loc, list):
         X_loc = [X_loc]
     if not isinstance(y_loc, list):
         y_loc = [y_loc]
+
     X = dataset.iloc[:, X_loc]
     y = dataset.iloc[:, y_loc]
 
-    if not supress_text:
-        print("All data is gathered begining to test")
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-    super_accuracy = Accuracy(X_loc, y_loc, 0, 0)
+    for i in range(1, max_degree):
+        polynomial_features = PolynomialFeatures(degree=i)
+        X_poly = polynomial_features.fit_transform(X)
+
+        model = LinearRegression()
+        model.fit(X_poly, y)
+        y_poly_pred = model.predict(X_poly)
+
+        rmse = np.sqrt(mean_squared_error(y, y_poly_pred))
+        r2 = r2_score(y, y_poly_pred)
+
+        if accuracy.accuracy_score < r2:
+            accuracy.remake(X_loc, y_loc, i, r2)
+
+    return accuracy.printable()
+
+
+def linear_regression(file, X_loc, y_loc):
+    dataset = clean_data(file)
+
+    if not isinstance(X_loc, list):
+        X_loc = [X_loc]
+    if not isinstance(y_loc, list):
+        y_loc = [y_loc]
+
+    X = dataset.iloc[:, X_loc]
+    y = dataset.iloc[:, y_loc]
+
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+    regr = LinearRegression()
+
+    regr.fit(X_train, y_train)
+    y_pred = regr.predict(X_test)
+
+    mean_squared_error(y_test, y_pred)
+    a = Accuracy(X_loc, y_loc, 1, r2_score(y_test, y_pred))
+    return a.printable()
 
 
 if __name__ == "__main__":
-    train_data = 'Data/mobile-price-classification/train.csv'
+    columns = [
+        "battery_power", "blue", "clock_speed", "dual_sim", "fc", "four_g", "int_memory", "m_dep", "mobile_wt",
+        "n_cores", "pc", "px_height", "px_width", "ram", "sc_h", "sc_w", "talk_time", "three_g", "touch_screen", "wifi",
+        "price_range"
+    ]
